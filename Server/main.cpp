ff35a378ba70c8ea12dbd9d3e8d2668ab2f09152
EC2 Default User <ec2-user@ip-172-31-42-187.us-west-2.compute.internal>
2014-04-05 21:50:31 +0000

    Edit the ZMQ to use the cpp bindings

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-#include <zmq.h>
+#include "zmq.hpp"
+#include <cassert>
 int main()
 {
    std::cout << "hi" << std::endl;
+   //Say hi
+   zmq::context_t context (1);
+   zmq::socket_t socket (context, ZMQ_REP);
+   socket.bind ("tcp://eth0:5867");
+   
+   while(true)
+   {
+      zmq::message_t message;
+      socket.recv (&request);
+      //Wait for new connection
+      //spin new connection in its own thread
+   }
    return 0;
 }
