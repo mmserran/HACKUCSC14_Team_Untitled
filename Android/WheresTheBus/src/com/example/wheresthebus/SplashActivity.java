1631a37cd6fd73b9f850221dd2ae73e190ab8e53
Mark Anthony Serrano <Mark.Serrano2@yahoo.com>
2014-04-05 16:46:58 -0700

    Added .9 button template

--- Android/WheresTheBus/src/com/example/wheresthebus/SplashActivity.java
+++ Android/WheresTheBus/src/com/example/wheresthebus/SplashActivity.java
@@ -1,8 +1,15 @@
 package com.example.wheresthebus;
+// Mentor email: robert@civinomics.com
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class SplashActivity extends Activity {
 
@@ -10,6 +17,19 @@ public class SplashActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Initiate the Upload Button
+		Button uploadBtn = (Button) findViewById(R.id.uploadBtn);
+		uploadBtn.setTextColor(Color.WHITE);
+		uploadBtn.setBackgroundResource(R.drawable.green_btn_statelist);
+		uploadBtn.setGravity(Gravity.CENTER);
+		//uploadBtn.setTextSize(24);
+		uploadBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// Perform action on click
+				
+			}
+		});
 	}
 
 	@Override
