4ee17a0902ab33fb6ea1cc2c29a7a0a34175a05f
Mark Anthony Serrano <Mark.Serrano2@yahoo.com>
2014-04-06 08:11:15 -0700

    Spinners implemented

--- Android/WheresTheBus/src/com/example/wheresthebus/SplashActivity.java
+++ Android/WheresTheBus/src/com/example/wheresthebus/SplashActivity.java
@@ -11,6 +11,7 @@ import java.net.UnknownHostException;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
@@ -20,13 +21,9 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class SplashActivity extends Activity {
 	
-	private Location currentBestLocation = null;
-	static final int TWO_MINUTES = 500 * 60;
-
 	private Socket socket;
 
 	private static final int SERVERPORT = 5867;
@@ -59,9 +56,25 @@ public class SplashActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_splash);
 
 		new Thread(new ClientThread()).start();
+		
+		// Initiate the Map Button
+		Button mapBtn = (Button) findViewById(R.id.mapBtn);
+		mapBtn.setTextColor(Color.WHITE);
+		mapBtn.setBackgroundResource(R.drawable.green_btn_statelist);
+		mapBtn.setGravity(Gravity.CENTER);
+		//uploadBtn.setTextSize(24);
+		mapBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				// Open Map window on click
+				Intent intent = new Intent(v.getContext(), MapActivity.class);
+				v.getContext().startActivity(intent);
+				
+			}
+		});
 		
 		// Initiate the Upload Button
 		Button uploadBtn = (Button) findViewById(R.id.uploadBtn);
@@ -73,147 +86,18 @@ public class SplashActivity extends Activity {
 			public void onClick(View v) {
 				
 				// Perform action on click
-				TextView test = (TextView) findViewById(R.id.testArea);
-				
-				// check if GPS is enabled or not
-				
-				Location coords = getLastBestLocation();
-				double latty = coords.getLatitude();
-				double longy = coords.getLongitude();
-				String str = Double.toString(latty) + "*" + Double.toString(longy);
-				str = "S1.0003*4.000*CONGRATULATIONS*18294839284928*Test*";
-				test.setText(str);
-				
-				// Format data recognized by server
-				
-				// Encrypt data?
-				
-				// Send data to server
-				try {
-					PrintWriter out = new PrintWriter(new BufferedWriter(
-							new OutputStreamWriter(socket.getOutputStream())),
-							true);
-					out.println(str);
-				} catch (UnknownHostException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				Intent intent = new Intent(v.getContext(), UploadActivity.class);
+				v.getContext().startActivity(intent);
 				
 			}
 		});
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-
-	/**
-	 * @return the last know best location
-	 * Source: http://stackoverflow.com/questions/1513485/how-do-i-get-the-current-gps-location-programmatically-in-android
-	 */
-	private Location getLastBestLocation() {
-		LocationManager mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		Location locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	    Location locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
-	    long GPSLocationTime = 0;
-	    if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
-
-	    long NetLocationTime = 0;
-
-	    if (null != locationNet) {
-	        NetLocationTime = locationNet.getTime();
-	    }
-
-	    if ( 0 < GPSLocationTime - NetLocationTime ) {
-	        return locationGPS;
-	    }
-	    else {
-	        return locationNet;
-	    }
-	}
-	
-	//@Override
-	public void onLocationChanged(Location location) {
-
-	    makeUseOfNewLocation(location);
-
-	    if(currentBestLocation == null){
-	        currentBestLocation = location;
-	    }
-	}
-	
-	/**
-	 * This method modify the last know good location according to the arguments.
-	 *
-	 * @param location The possible new location.
-	 */
-	void makeUseOfNewLocation(Location location) {
-	    if ( isBetterLocation(location, currentBestLocation) ) {
-	        currentBestLocation = location;
-	    }
-	}
-	
-	/** Determines whether one location reading is better than the current location fix
-	 * @param location  The new location that you want to evaluate
-	 * @param currentBestLocation  The current location fix, to which you want to compare the new one.
-	 * Source: http://stackoverflow.com/questions/1513485/how-do-i-get-the-current-gps-location-programmatically-in-android
-	 */
-	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
-	    if (currentBestLocation == null) {
-	        // A new location is always better than no location
-	        return true;
-	    }
-
-	    // Check whether the new location fix is newer or older
-	    long timeDelta = location.getTime() - currentBestLocation.getTime();
-	    boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
-	    boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
-	    boolean isNewer = timeDelta > 0;
-
-	    // If it's been more than two minutes since the current location, use the new location,
-	    // because the user has likely moved.
-	    if (isSignificantlyNewer) {
-	        return true;
-	        // If the new location is more than two minutes older, it must be worse.
-	    } else if (isSignificantlyOlder) {
-	        return false;
-	    }
-
-	    // Check whether the new location fix is more or less accurate
-	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
-	    boolean isLessAccurate = accuracyDelta > 0;
-	    boolean isMoreAccurate = accuracyDelta < 0;
-	    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
-
-	    // Check if the old and new location are from the same provider
-	    boolean isFromSameProvider = isSameProvider(location.getProvider(),
-	                                                currentBestLocation.getProvider());
-
-	    // Determine location quality using a combination of timeliness and accuracy
-	    if (isMoreAccurate) {
-	        return true;
-	    } else if (isNewer && !isLessAccurate) {
-	        return true;
-	    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
-	        return true;
-	    }
-	    return false;
-	}
-
-	/** Checks whether two providers are the same 
-	 * Source: http://stackoverflow.com/questions/1513485/how-do-i-get-the-current-gps-location-programmatically-in-android
-	 */
-	private boolean isSameProvider(String provider1, String provider2) {
-	    if (provider1 == null) {
-	        return provider2 == null;
-	    }
-	    return provider1.equals(provider2);
 	}
 }
---
+++ Android/WheresTheBus/src/com/example/wheresthebus/UploadActivity.java
@@ -0,0 +1,240 @@
+package com.example.wheresthebus;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class UploadActivity extends Activity {
+
+	private Location currentBestLocation = null;
+	static final int TWO_MINUTES = 500 * 60;
+	
+	private Spinner spinner1, spinner2;
+	String [] routesList, directionList;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_upload);
+
+		TextView title = (TextView) findViewById(R.id.uploadTitle);
+		title.setTextSize(24);
+		
+		routesList = getResources().getStringArray(R.array.routes);
+		
+		ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<String>
+        (this, android.R.layout.simple_spinner_item, routesList);
+		
+		directionList = getResources().getStringArray(R.array.directions);
+		
+		ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>
+        (this, android.R.layout.simple_spinner_item, directionList);
+         
+		dataAdapter1.setDropDownViewResource
+		        (android.R.layout.simple_spinner_dropdown_item);
+
+		dataAdapter2.setDropDownViewResource
+		        (android.R.layout.simple_spinner_dropdown_item);
+		
+        spinner1 = (Spinner) findViewById(R.id.busRouteSpinner);
+		spinner1.setAdapter(dataAdapter1);
+		
+        spinner2 = (Spinner) findViewById(R.id.directionSpinner);
+		spinner2.setAdapter(dataAdapter2);
+		
+		// Spinner item selection Listener  
+		addListenerOnSpinnerItemSelection();
+		
+		// Button click Listener 
+		addListenerOnButton();
+		
+		// Initiate the Map Button
+		final Button uploadBtn = (Button) findViewById(R.id.uploadBtn);
+		uploadBtn.setTextColor(Color.WHITE);
+		uploadBtn.setBackgroundResource(R.drawable.green_btn_statelist);
+		uploadBtn.setGravity(Gravity.CENTER);
+		//uploadBtn.setTextSize(24);
+		uploadBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				// Open Map window on click
+				Location coords = getLastBestLocation();
+				double latty = coords.getLatitude();
+				double longy = coords.getLongitude();
+				String testMsg = Double.toString(latty) + "*" + Double.toString(longy);
+				uploadBtn.setText(testMsg);
+				
+			}
+		});
+		
+	}
+
+	// Add spinner data
+    
+    public void addListenerOnSpinnerItemSelection(){
+         
+                spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+     
+    //get the selected dropdown list value
+     
+    public void addListenerOnButton() {
+ 
+        spinner1 = (Spinner) findViewById(R.id.busRouteSpinner);
+         
+        final Button uploadBtn = (Button) findViewById(R.id.uploadBtn);
+ 
+        uploadBtn.setOnClickListener(new OnClickListener() {
+ 
+            @Override
+            public void onClick(View v) {
+ 
+            	uploadBtn.setText(String.valueOf(spinner1.getSelectedItem()));
+            }
+ 
+        });
+ 
+    }
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.upload, menu);
+		return true;
+	}
+
+	/**
+	 * @return the last know best location
+	 * Source: http://stackoverflow.com/questions/1513485/how-do-i-get-the-current-gps-location-programmatically-in-android
+	 */
+	private Location getLastBestLocation() {
+		LocationManager mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Location locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	    Location locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+	    long GPSLocationTime = 0;
+	    if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
+
+	    long NetLocationTime = 0;
+
+	    if (null != locationNet) {
+	        NetLocationTime = locationNet.getTime();
+	    }
+
+	    if ( 0 < GPSLocationTime - NetLocationTime ) {
+	        return locationGPS;
+	    }
+	    else {
+	        return locationNet;
+	    }
+	}
+	
+	//@Override
+	public void onLocationChanged(Location location) {
+
+	    makeUseOfNewLocation(location);
+
+	    if(currentBestLocation == null){
+	        currentBestLocation = location;
+	    }
+	}
+	
+	/**
+	 * This method modify the last know good location according to the arguments.
+	 *
+	 * @param location The possible new location.
+	 */
+	void makeUseOfNewLocation(Location location) {
+	    if ( isBetterLocation(location, currentBestLocation) ) {
+	        currentBestLocation = location;
+	    }
+	}
+	
+	/** Determines whether one location reading is better than the current location fix
+	 * @param location  The new location that you want to evaluate
+	 * @param currentBestLocation  The current location fix, to which you want to compare the new one.
+	 * Source: http://stackoverflow.com/questions/1513485/how-do-i-get-the-current-gps-location-programmatically-in-android
+	 */
+	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+	    if (currentBestLocation == null) {
+	        // A new location is always better than no location
+	        return true;
+	    }
+
+	    // Check whether the new location fix is newer or older
+	    long timeDelta = location.getTime() - currentBestLocation.getTime();
+	    boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+	    boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+	    boolean isNewer = timeDelta > 0;
+
+	    // If it's been more than two minutes since the current location, use the new location,
+	    // because the user has likely moved.
+	    if (isSignificantlyNewer) {
+	        return true;
+	        // If the new location is more than two minutes older, it must be worse.
+	    } else if (isSignificantlyOlder) {
+	        return false;
+	    }
+
+	    // Check whether the new location fix is more or less accurate
+	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+	    boolean isLessAccurate = accuracyDelta > 0;
+	    boolean isMoreAccurate = accuracyDelta < 0;
+	    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+	    // Check if the old and new location are from the same provider
+	    boolean isFromSameProvider = isSameProvider(location.getProvider(),
+	                                                currentBestLocation.getProvider());
+
+	    // Determine location quality using a combination of timeliness and accuracy
+	    if (isMoreAccurate) {
+	        return true;
+	    } else if (isNewer && !isLessAccurate) {
+	        return true;
+	    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	/** Checks whether two providers are the same 
+	 * Source: http://stackoverflow.com/questions/1513485/how-do-i-get-the-current-gps-location-programmatically-in-android
+	 */
+	private boolean isSameProvider(String provider1, String provider2) {
+	    if (provider1 == null) {
+	        return provider2 == null;
+	    }
+	    return provider1.equals(provider2);
+	}
+	
+	public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+		 
+	    public void onItemSelected(AdapterView<?> parent, View view, int pos,
+	            long id) {
+	         
+	        Log.d("HERE", "On Item Select : \n" + parent.getItemAtPosition(pos).toString());
+	    }
+	 
+	    @Override
+	    public void onNothingSelected(AdapterView<?> arg0) {
+	        // TODO Auto-generated method stub
+	 
+	    }
+	 
+	}
+}
+
