c7fa4414bea37327e29ce58ec8cb733b17de9624
Mark Anthony Serrano <Mark.Serrano2@yahoo.com>
2014-04-06 15:26:30 -0700

    Contains many debug statements

--- Android/WheresTheBus/src/com/example/wheresthebus/MapActivity.java
+++ Android/WheresTheBus/src/com/example/wheresthebus/MapActivity.java
@@ -1,7 +1,10 @@
 package com.example.wheresthebus;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
@@ -12,6 +15,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MapActivity extends Activity {
 
@@ -23,7 +27,6 @@ public class MapActivity extends Activity {
 	/*
 	 * Source: http://examples.javacodegeeks.com/android/core/socket-core/android-socket-example/
 	 */
-	
 	class ClientThread implements Runnable {
 
 		@Override
@@ -33,13 +36,16 @@ public class MapActivity extends Activity {
 				InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
 
 				socket = new Socket(serverAddr, SERVERPORT);
-				Log.e("Connected", "Connected");
+				Log.d("socket", socket.isConnected()==true ? "true" : "false");
+				Log.e("Connected", "Connected to server");
 				
 			} catch (UnknownHostException e1) {
 				e1.printStackTrace();
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
+
+			Log.e("Exited", "exited client thread");
 
 		}
 
@@ -51,16 +57,49 @@ public class MapActivity extends Activity {
         setContentView(R.layout.activity_map);
 
 		new Thread(new ClientThread()).start();
+		Log.d("debug", "ClientThread set up");
 		
 		Integer num = 1;
 		String requestStr = 'G' + Integer.toString(num);
 		
+		String line;
+		TextView MsgFromServer = (TextView) findViewById(R.id.testArea);
+		
 		// Send data to server
+		OutputStreamWriter outputStream = null;
+
+		Log.d("debug", "BufferedReader in1");
 		try {
-			PrintWriter out = new PrintWriter(new BufferedWriter(
-					new OutputStreamWriter(socket.getOutputStream())),
-					true);
+			Log.d("debug", "asdfasdf1");
+			OutputStream outStr = socket.getOutputStream();
+			Log.d("debug", "asdfasdf2");
+			outputStream = new OutputStreamWriter(outStr);
+			Log.d("debug", "asdfasdf3");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		Log.d("debug", "BufferedReader in2");
+		BufferedWriter buffWriter = new BufferedWriter(outputStream);
+
+		Log.d("debug", "BufferedReader in3");
+		try {
+			PrintWriter out = new PrintWriter(buffWriter, true);
+			
+			
 			out.println(requestStr);
+
+			Log.d("debug", "BufferedReader in4");
+
+			final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while((line=in.readLine())!=null){
+                MsgFromServer.append(line);
+        		Log.d("DATA!!", "Received Data from Server");
+            }
+			Log.d("debug", "BufferedReader in5");
+                
+                
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -69,6 +108,10 @@ public class MapActivity extends Activity {
 			e.printStackTrace();
 		}
 		
+		// Wait for inbound packet
+
+		
+		// Parse and display data
 		
         
     }
