532998c1da292c9d91885c1e1cb087eb3c4408c1
EC2 Default User <ec2-user@ip-172-31-42-187.us-west-2.compute.internal>
2014-04-06 00:38:06 +0000

    Add string delimiter parsing for message packet

--- Server/main.cpp
+++ Server/main.cpp
@@ -17,8 +17,36 @@ int main()
       std::cout << "received data!"<<std::endl;
       std::cout << messageString << std::endl;
       
+      std::string rest = messageString;
+      std::string data[5];
+      for(int i = 0; i < 5; i++)
+      {
+         size_t delimLocation = rest.find('*');
+         if(delimLocation == std::string::npos) //no delim found
+         {
+            //throw error
+         }
+         else
+         {
+            data[i] = rest.substr(0, delimLocation); //sets data[i] from begin
+                                                     // to next '*' character
+            rest = rest.substr(delimLocation+1); //sets rest to everything after
+                                               //the first '*'
+            std::cout << "Part " << i << ": "<<data[i] << std::endl;
+         }
+      }
+      //double lati, longi;
+      //std::string route;
+     //long upTime;
+      //char direction;
       
+      //data[0] >> lati;
+      //data[1] >> longi;
+      //route = data[2];
+      //data[3] >> upTime;
+      //data[4] >> direction;
       
+      //someHandlerFunction(lati, longi, route, upTime, direction); //should thread and free this to make connections
       
       zmq::message_t reply (50);
       memcpy ((void *) reply.data (), "HWORLD DUDE", 40);
